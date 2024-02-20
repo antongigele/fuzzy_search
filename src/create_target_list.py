a3f460ac92107c5eebfbadc7286ae38b1ffc4092
antongigele <anton.gigele@aon.at>
2024-02-20 14:46:37 +0100

    generalisation of target_list function start

--- src/create_target_list.py
+++ src/create_target_list.py
@@ -25,24 +25,7 @@ def dissolve_subsets_into_set(input_set):
     return {string.strip() for string in new_set}
 
 def create_target_list():
-    ## Load clusters data for cluster names#
-    clusters_dict = load_json2data(output_path, "combined_atom_clusters.json")
-    #-------------------------------------------------------------------
-    # studios
-    studio_names_tnaflix = pd.read_csv(path + "studios.csv", delimiter=";")
-    studios_names, studios_names_atomized = get_atoms_from_col(studio_names_tnaflix, "title")
-
-    #-------------------------------------------------------------------
-    # pornstars
-    pornstar_names_tnaflix = pd.read_csv(path + "pornstars.csv")
-    pornstar_names, pornstar_names_atomized = get_atoms_from_col(pornstar_names_tnaflix, "name")
-    pornstar_aliases, pornstar_aliases_atomized = get_atoms_from_col(pornstar_names_tnaflix, "aliases")
-    pornstar_aliases = dissolve_subsets_into_set(pornstar_aliases)
-
-    #-------------------------------------------------------------------
-
-    final_target_list = list(clusters_dict.keys()) + list(pornstar_names) + list(studios_names)
-    export_list2csv(data_source_path, "final_target_list.csv", final_target_list)
+    pass
 
 if __name__ == "__main__":
     create_target_list()
